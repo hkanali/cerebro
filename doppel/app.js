1f314d4eb96c5a80124b7cd7bafcc33895e5d702
金井 晴弘 <haruhiro.kanai2308@gmail.com>
2015-05-06 10:13:50 +0900

    フォーマットとか

--- doppel/app.js
+++ doppel/app.js
@@ -14,7 +14,7 @@ var main = require('./main');
 var app = express();
 
 // 1時間毎に叩き起こす
-heroku.init('https://cerebro1989.herokuapp.com/', 1 * 60 * 60 * 1000);
+//heroku.init('https://cerebro1989.herokuapp.com/', 1 * 60 * 60 * 1000);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,9 +33,9 @@ app.use('/instagram', instagram);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -43,23 +43,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
