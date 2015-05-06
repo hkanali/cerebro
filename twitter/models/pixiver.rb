de1567ce68d1fae62d7c8d69a97eb15d41475e25
chck <iwazaki_yuki@cyberagent.co.jp>
2015-05-06 23:52:34 +0900

    add sns-matchmaker

---
+++ twitter/models/pixiver.rb
@@ -0,0 +1,10 @@
+require 'active_record'
+require 'activerecord-import'
+require 'yaml'
+
+yaml_path = "#{__dir__}/../../config.yml"
+conf = YAML::load_file(yaml_path)
+ActiveRecord::Base.establish_connection(conf["mysql"]["prd"])
+# ActiveRecord::Base.establish_connection($conf["mysql"]["dev"])
+class Pixiver < ActiveRecord::Base
+end
